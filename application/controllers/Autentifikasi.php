9785f1e53ae23c8597f422ab9017af704e8966b5
diansri <diansrimilani123@gmail.com>
2022-11-13 17:04:09 +0700

    menambah tugas 9

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -1,24 +1,26 @@
 <?php
-defined('BASEPATH') or exit('No Direct script access allowed');
 
-class Autentifikasi extends CI_Controller {
-    public function index() {
-        //Jika statusnya sudah login, maka tidak bisa mengakses halaman login alias dikembalikan ke tampilan user
-        if ($this->session->userdata('email')) {
+class Autentifikasi extends CI_Controller
+{
+
+    Public function index()
+    {
+        //jika statusnya sudah login, maka tidak bisa mengakses halaman login alias dikembalikan ke tampilan user
+        if($this->session->userdata('email')){
             redirect('user');
         }
-
-        $this->form_validation->set_rules('email', 'Alamat Email', 'required|trim|valid_email', [
-            'required' => 'Email Harus diisi!!!',
-            'valid_email' => 'Email Tidak Benar'
+        
+        $this->form_validation->set_rules('email', 'Alamat email', 'required|trim|valid_email', [
+            'required' => 'Email Harus Diisi!!',
+            'valid_email' => 'Email Tidak Benar!!'
         ]);
         $this->form_validation->set_rules('password', 'Password', 'required|trim', [
-            'required' => 'password Harus diisi!!!'
+            'required' => 'Password Harus diisi'
         ]);
         if ($this->form_validation->run() == false) {
             $data['judul'] = 'Login';
             $data['user'] = '';
-            //Kata 'login' merupakan nilai dari variabel judul dalam array $data dikirimkan ke view aute_header
+            //kata 'login' merupakan nilai dari variabel judul dalam array $data dikirimkan ke view aute_header
             $this->load->view('templates/aute_header', $data);
             $this->load->view('autentifikasi/login');
             $this->load->view('templates/aute_footer');
@@ -27,15 +29,17 @@ class Autentifikasi extends CI_Controller {
         }
     }
 
-    private function _login() {
+    private function _login()
+    {
         $email = htmlspecialchars($this->input->post('email', true));
+
         $password = $this->input->post('password', true);
 
         $user = $this->ModelUser->cekData(['email' => $email])->row_array();
 
         //jika usernya ada
         if ($user) {
-            //jika user sudah aktif
+            //jika user sudah aktif 
             if ($user['is_active'] == 1) {
                 //cek password
                 if (password_verify($password, $user['password'])) {
@@ -50,64 +54,65 @@ class Autentifikasi extends CI_Controller {
                         redirect('admin');
                     } else {
                         if ($user['image'] == 'default.jpg') {
-                            $this->session->set_flashdata('pesan', '<div class="alert alert-info alert-message" role="alert">Silahkan Ubah Profile Anda untuk Ubah Photo Profil</div>');
+                            $this->session->set_flashdata('pesan', '<div class="alert alert-info alert-message" role="alert">Silahkan Ubah Profil Anda untuk Ubah Photo Profil</div>');
+
                         }
                         redirect('user');
                     }
                 } else {
-                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password Salah!!</div>');
+                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Password salah!!</div>');
                     redirect('autentifikasi');
                 }
             } else {
-                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">User belum diaktifasi!!</div>');
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
                 redirect('autentifikasi');
             }
-        } else {
-            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-message" role="alert">Email tidak terdaftar!!</div>');
-            redirect('autentifikasi');
         }
+
+
     }
 
-    public function logout() {
-        session_unset();
-        session_destroy();
-        redirect('autentifikasi');
+    public function blok()
+    {
+            $this->load->view('autentifikasi/blok');
     }
 
-    public function blok() {
-        $this->load->view('autentifikasi/blok');
+    public function gagal()
+    {
+            $this->load->view('autentifikasi/gagal');
     }
 
-    public function gagal() {
-        $this->load->view('autentifikasi/gagal');
-    }
-
-    public function registrasi() {
+    public function registrasi()
+    {
         if ($this->session->userdata('email')) {
             redirect('user');
         }
         //membuat rule untuk inputan nama agar tidak boleh kosong dengan membuat pesan error dengan
-        //bahasa sendiri yaitu 'Nama belum diisi'
+        //bahasa sendiri yaitu 'Nama Belum diisi'
         $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required', [
             'required' => 'Nama Belum diisi!!'
         ]);
-        //membuat rule untuk inputan email agar tidak boleh kosong tidak ada spasi, format email harus valid
+        //membuat rule untuk inputan email agar tidak boleh kosong, tidak ada spasi, format email harus valid
         //dan email belum pernah dipakai sama user lain dengan membuat pesan error dengan bahasa sendiri
-        //yaitu jika format email tidak benar maka pesannya 'Email tidak benar!!'. Jika email belum diisi,
-        //maka pesannya adalah 'email belum diisi', dan jika email yang diinput sudah dipakai user lain,
-        //maka pesannya 'email sudah dipakai'
-        $this->form_validation->set_rules('email', 'Alamat Email', 'required');
-        //membuat rule untuk inputan password agar tidak boleh kosong, tidak ada spasi, tidak boleh kurang dari
-        // dari 3 digit, dan password harus sama dengan repeat password tidak diinput sama, maka pesannya
-        //'Password tidak sama'. Jika Password diisi kurang dari 3 digit maka pesannya adalah
-        //'Password terlalu pendek'.
-        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
-            'matches' => 'Password tidak sama!!',
-            'min_length' => 'Password terlalu pendek'
+        //yaitu jika format email tidak benar maka pesannya 'Email yang diinput sudah dipakai user lain,
+        //maka pesannya 'Email Sudah dipakai'
+        $this->form_validation->set_rules('email', 'Alamat Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'valid_email' => 'Email Tidak Benar!!',
+            'required' => 'Email Belum diisi!!',
+            'is_unique' => 'Email Sudah Terdaftar!'
         ]);
-        $this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|min_length[3]|matches[password1]');
-        //jika tidak disubmit kemudian validasi form diatas tidak berjalan, maka akan tetap berada di
-        //tampilan registrasi. tapi jika disubmit kemudian validasi form diatas berjalan, maka data yang
+        //membuat rule untuk inputan password agar tidak boleh kosong, tidak ada spasi, tidak boleh kurang dari
+        //dari 3 digit, dan password harus sama dengan repeat password dengan membuat pesan error dengan 
+        //bahasa sendiri yaitu jika password dan repeat password tidak diinput sama, maka pesannya
+        //'Password Tidak Sama'. jika password diisi kurang dari 3 digit, maka pesannya adalah 
+        //'Password Terlalu Pendek'.
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+                'matches' => 'Password Tidak Sama!!',
+                'min_length' => 'Password Terlalu Pendek'
+        ]);
+        $this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|matches[password1]');
+        //jika jida disubmit kemudian validasi form diatas tidak berjalan, maka akan tetap berada di
+        //tampilan registrasi. tapi jika disubmit kemudian validasi form diatas berjalan, maka data yang 
         //diinput akan disimpan ke dalam tabel user
         if ($this->form_validation->run() == false) {
             $data['judul'] = 'Registrasi Member';
@@ -128,10 +133,9 @@ class Autentifikasi extends CI_Controller {
 
             $this->ModelUser->simpanData($data); //menggunakan model
 
-            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Selamat!! akun member anda sudah dibuat. Silahkan aktivasi akun anda</div>');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Selamat!! akun member anda sudah dibuat. Silahkan Aktivasi Akun anda</div>');
             redirect('autentifikasi');
         }
+
     }
 }
-
-?>
